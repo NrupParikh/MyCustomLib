3a55058e84dca043d94274d1916ca9e8db9d2b91
NrupParikh <108717119+NrupParikh@users.noreply.github.com>
2024-02-21 18:32:17 +0530

    - Test commit

--- app/src/main/java/com/nrup/mysampleapp/MainActivity.kt
+++ app/src/main/java/com/nrup/mysampleapp/MainActivity.kt
@@ -2,17 +2,15 @@ package com.nrup.mysampleapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.TextView
-import com.nrup.nruplib.GetMyProfileDetails
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val tvHello = findViewById<TextView>(R.id.textView)
-        tvHello.text = GetMyProfileDetails.getMyDetails()
-        Log.d("TAG","Data"+GetMyProfileDetails.getMyDetails())
+//        val tvHello = findViewById<TextView>(R.id.textView)
+//        tvHello.text = GetMyProfileDetails.getMyDetails()
+//        Log.d("TAG","Data"+GetMyProfileDetails.getMyDetails())
     }
 }
